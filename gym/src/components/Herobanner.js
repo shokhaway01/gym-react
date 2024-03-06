dd4fa194ed891d2f592dd4f6d585b8156e9148b6
shokhaway01 <shoxauey@gmail.com>
2024-03-06 05:52:58 +0500

    new commit(searchbar)

--- gym/src/components/Herobanner.js
+++ gym/src/components/Herobanner.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box,Stack,Typography,Button } from "@mui/material"
-
+import HerobannerImage from '../assets/images/banner.png'
 
 const Herobanner = () => {
   return (
@@ -35,18 +35,47 @@ const Herobanner = () => {
                 }
             }
         }
-        >
+        mb={'23px'}
+        mt={'30px'}
+       >
             Sweat, Smile <br/> and Repeat
         </Typography>
 
-        <Typography fontSize="22px" lineHeight="35px">
+        <Typography fontSize="22px" lineHeight="35px" mb={4}>
             Check out the most effective exercises
         </Typography>
 
 
-        <Button variant='contained' color='error'>
+        <Button variant='contained' color='error' href='#exercises'
+            sx={
+                {
+                    backgroundColor:'#FF2526',
+                    padding:'15px',
+                }
+            }
+        >
             Explore Exercises
         </Button>
+        
+        <Typography
+            fontWeight={700}
+            color="#ff2625"
+            fontSize={'200px'}
+            letterSpacing={'4px'}
+            sx={
+                {
+                    opacity:0.1,
+                    display:{
+                        lg:'block',
+                        xs:'none'
+                    }
+                }
+            }
+        >
+            Exercises
+        </Typography>
+        <img src={HerobannerImage} alt='banner' className='hero-banner-img'/>
+
     </Box>
   )
 }
--- gym/src/components/SearchExercises.js
+++ gym/src/components/SearchExercises.js
@@ -1,8 +1,70 @@
+import { useEffect, useState } from "react"
 import React from 'react'
+import { Box,Button,Stack,TextField,Typography } from "@mui/material"
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack
+        alignItems={'center'}
+        mt={'37px'}
+        justifyContent={'center'}
+        p={'20px'}
+    >
+        <Typography
+            textAlign={'center'}
+            fontWeight={'700'}
+            sx={{fontSize:{lg:'44px',xs:'30px'}}}
+            mb={'50px'}
+>
+            Awesome Exercises You <br/>Should Know
+        </Typography>
+        <Box position={'relative'} mb={'72px'}>
+            <TextField
+                sx={
+                {
+                    input:{
+                        fontWeight:'700',
+                        border:'none',
+                        borderRadius:'5px'
+                    },
+
+                    width:{
+                        lg:"800px",
+                        xs:"350px"
+                    },
+                    backgroundColor:"#fff",
+                    borderRadius:'40px'
+                }
+                }
+                height='76px'
+                value={''}
+                onChange={(e)=>{}}
+                placeholder="search exercises"
+                type="text"
+            />
+            <Button className="search-btn"
+                sx={
+                    {
+                        backgroundColor:"#ff2625",
+                        color:'#fff',
+                        textTransform:'none',
+                        width:{
+                            lg:'175px',
+                            xs:'880px'
+                        },
+                        fontSize:{
+                            lg:'20px',
+                            xs:'14px'
+                        },
+                        height:'56px',
+                        position:"absolute"
+                    }
+                }
+            >
+                Button
+            </Button>
+        </Box>
+    </Stack>
   )
 }
 
