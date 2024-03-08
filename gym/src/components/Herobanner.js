d2a705469daf842bc781cd29f78b621fc3741fbd
shokhaway01 <shoxauey@gmail.com>
2024-03-09 00:15:09 +0500

    NEW UPDATE

--- gym/src/components/Herobanner.js
+++ gym/src/components/Herobanner.js
@@ -1,84 +1,26 @@
-import React from 'react'
-import { Box,Stack,Typography,Button } from "@mui/material"
-import HerobannerImage from '../assets/images/banner.png'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Herobanner = () => {
-  return (
-    <Box sx={
-        {
-        mt: {
-            lg: '212px',
-            xs:'70px'
-           },
-    
-        ml: {
-            sm:'50px'
-        }
-        
-        }
-    } position="relative" p="20px">
-        <Typography 
-        color="#FF2625"
-        fontWeight="600"
-        fontSize="26px"
-        >
-            Fitness Club
-        </Typography>
+import HeroBannerImage from '../assets/images/banner.png';
 
-        <Typography 
-        fontWeight="680"
-        sx={
-            {
-                fontSize: {
-                    lg: '44px',
-                    xs: '40px'
-                }
-            }
-        }
-        mb={'23px'}
-        mt={'30px'}
-       >
-            Sweat, Smile <br/> and Repeat
-        </Typography>
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Sweat, Smile <br />
+      And Repeat
+    </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Exercise
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
 
-        <Typography fontSize="22px" lineHeight="35px" mb={4}>
-            Check out the most effective exercises
-        </Typography>
-
-
-        <Button variant='contained' color='error' href='#exercises'
-            sx={
-                {
-                    backgroundColor:'#FF2526',
-                    padding:'15px',
-                }
-            }
-        >
-            Explore Exercises
-        </Button>
-        
-        <Typography
-            fontWeight={700}
-            color="#ff2625"
-            fontSize={'200px'}
-            letterSpacing={'4px'}
-            fontFamily={''}
-            sx={
-                {
-                    opacity:0.1,
-                    display:{
-                        lg:'block',
-                        xs:'none'
-                    }
-                }
-            }
-        >
-            Shohjaxon
-        </Typography>
-        <img src={HerobannerImage} alt='banner' className='hero-banner-img'/>
-
-    </Box>
-  )
-}
-
-export default Herobanner
+export default HeroBanner;
